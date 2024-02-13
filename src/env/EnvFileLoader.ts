f7be74c533fe39ac67ddc31cc9e5e67d81738f6d
majortom327 <me@valentin-thomas.com>
2024-02-12 20:21:04 -0800

    Update package.json version and handle file not found error in EnvFileLoader

--- src/env/EnvFileLoader.ts
+++ src/env/EnvFileLoader.ts
@@ -1,6 +1,6 @@
 import * as fs from "fs";
 import * as path from "path";
-import { assoc, clone, compose, isEmpty, isNil, reduce, reject } from "rambda";
+import { assoc, clone, compose, isEmpty, isNil, keys, reduce, reject } from "rambda";
 import { P, match } from "ts-pattern";
 
 export class EnvFileLoader {
@@ -32,8 +32,18 @@ export class EnvFileLoader {
       .with({ config: P.string }, ({ config }) => config)
       .otherwise(() => path.join(process.cwd(), ".env")) as string;
 
-    const env = fs.readFileSync(fileToLoad, "utf-8");
-    const lines = env.split("\n");
+      let lines: string[] = []
+
+    try {
+      const env = fs.readFileSync(fileToLoad, "utf-8");
+      lines = env.split("\n");
+    }
+    catch (e) {
+      lines = keys(process.env).map((key) => {
+        return (`${key}=${process.env[key]}`);
+      })
+    }
+
     const loadedEnv = compose(
       reduce((acc, line: string) => {
         const parserLine = this.parseRow(line);
